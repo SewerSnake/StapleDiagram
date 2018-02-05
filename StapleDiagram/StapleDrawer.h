de0e3aac815ec6a3a5f3dd9a29812944a10fef5c
SewerSnake <ericgroseclos@hotmail.com>
2018-02-05 09:30:42 +0100

    Created a View object and connected class StapleDrawer to it.

---
+++ StapleDiagram/StapleDrawer.h
@@ -0,0 +1,13 @@
+//
+//  StapleDrawer.h
+//  StapleDiagram
+//
+//  Created by Eric Groseclos on 2018-02-05.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StapleDrawer : UIView
+
+@end
---
+++ StapleDiagram/StapleDrawer.m
@@ -0,0 +1,21 @@
+//
+//  StapleDrawer.m
+//  StapleDiagram
+//
+//  Created by Eric Groseclos on 2018-02-05.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import "StapleDrawer.h"
+
+@implementation StapleDrawer
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+@end
